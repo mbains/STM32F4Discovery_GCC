c3e8ee24cc64bcb5ae5ffdc44223d95da0f8d342
mbains <manjinder.bains@gmail.com>
2015-06-14 12:09:11 -0700

    working GPIO

--- blink_led/main.c
+++ blink_led/main.c
@@ -3,42 +3,61 @@
 #include <stm32f4xx_gpio.h>
 
 void Delay(uint32_t nTime);
+const uint16_t LEDS = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
+const uint16_t LED[4] = {GPIO_Pin_12, GPIO_Pin_13, GPIO_Pin_14, GPIO_Pin_15};
+
+int main(void) {
+
+    SystemCoreClockUpdate();
+
+    GPIO_InitTypeDef GPIO_InitStructure;
+    GPIO_StructInit(&GPIO_InitStructure);
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+
+    GPIO_InitStructure.GPIO_Pin = LEDS;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_Init(GPIOD, &GPIO_InitStructure);
 
 
-int main(void){
-    
-    
-    if(SysTick_Config(SystemCoreClock / 1000))
-        while(1);
-    
-    while(1) {
+    if (SysTick_Config(SystemCoreClock / 1000))
+        while (1);
 
-        
+    while (1) {
+        static uint32_t counter = 0;
+
+        ++counter;
+
+        GPIO_ResetBits(GPIOD, LEDS);
+
+        GPIO_SetBits(GPIOD, LED[counter % 4]);
         Delay(250);
+
     }
     return 0;
 }
 
 /*(5) Timer code*/
 static __IO uint32_t TimingDelay;
-void Delay(uint32_t nTime) 
-{
+
+void Delay(uint32_t nTime) {
     TimingDelay = nTime;
-    while(TimingDelay !=0);
+    while (TimingDelay != 0);
 }
 
-void SysTick_Handler(void) 
-{
-    if(TimingDelay != 0x00) 
+void SysTick_Handler(void) {
+    if (TimingDelay != 0x00)
         TimingDelay--;
 }
 
 #ifdef USE_FULL_ASSERT 
-void assert_failed(uint8_t * file, uint32_t line)
-{
+
+void assert_failed(uint8_t * file, uint32_t line) {
     /* Infinite loop*/
     /* use GDB to find where we're here*/
-    while(1);
+    while (1);
 }
 #endif
 
